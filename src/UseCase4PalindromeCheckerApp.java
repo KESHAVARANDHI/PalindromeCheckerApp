df868e1b0060067c32c1f8c6ebccd5264a3024a7
KESHAVARANDHI <your GitHub email>
2026-02-23 16:14:30 +0530

    Character Array Based Palindrome Check

---
+++ src/UseCase4PalindromeCheckerApp.java
@@ -0,0 +1,33 @@
+public class UseCase4PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String word = "madam";
+
+        // Convert string to character array
+        char[] characters = word.toCharArray();
+
+        // Two-pointer approach
+        int start = 0;
+        int end = characters.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println("The word \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The word \"" + word + "\" is NOT a Palindrome.");
+        }
+    }
+}
